79b62ef86dd75b5b524ac270388f6170a2cc583a
Lilit <lily.muradyan91@gmail.com>
2015-06-01 16:40:43 +0400

    made with fragment

--- app/src/main/java/com/example/lilit/web/ListViewFragment.java
+++ app/src/main/java/com/example/lilit/web/ListViewFragment.java
@@ -1,19 +1,194 @@
 package com.example.lilit.web;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Lilit on 6/1/15.
  */
 public class ListViewFragment extends Fragment {
-    @Nullable
+    private AnimatedExpandableListView listView;
+    private ExampleAdapter adapter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.group_item, null);
+        return inflater.inflate(R.layout.fragment_list_view, null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        List<GroupItem> items = new ArrayList<GroupItem>();
+        for(int i = 1; i < 100; i++) {
+            GroupItem item = new GroupItem();
+
+            item.title = "Group " + i;
+
+            for(int j = 0; j < i; j++) {
+                ChildItem child = new ChildItem();
+                child.title = "Awesome item " + j;
+                child.hint = "Too awesome";
+
+                item.items.add(child);
+            }
+
+            items.add(item);
+        }
+
+        adapter = new ExampleAdapter(getActivity());
+        adapter.setData(items);
+
+        listView = (AnimatedExpandableListView) getView().findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+        // In order to show animations, we need to use a custom click handler
+        // for our ExpandableListView.
+        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                // We call collapseGroupWithAnimation(int) and
+                // expandGroupWithAnimation(int) to animate group
+                // expansion/collapse.
+                if (listView.isGroupExpanded(groupPosition)) {
+                    listView.collapseGroupWithAnimation(groupPosition);
+                } else {
+                    listView.expandGroupWithAnimation(groupPosition);
+                }
+                return true;
+            }
+
+        });
+    }
+
+    private static class GroupItem {
+        String title;
+        List<ChildItem> items = new ArrayList<ChildItem>();
+    }
+
+    private static class ChildItem {
+        String title;
+        String hint;
+    }
+
+    private static class ChildHolder {
+        TextView title;
+        TextView hint;
+    }
+
+    private static class GroupHolder {
+        TextView title;
+        ImageView imageView;
+    }
+
+    /**
+     * Adapter for our list of {@link GroupItem}s.
+     */
+    private class ExampleAdapter extends AnimatedExpandableListView.AnimatedExpandableListAdapter {
+        private LayoutInflater inflater;
+
+        private List<GroupItem> items;
+
+        public ExampleAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+        }
+
+        public void setData(List<GroupItem> items) {
+            this.items = items;
+        }
+
+        @Override
+        public ChildItem getChild(int groupPosition, int childPosition) {
+            return items.get(groupPosition).items.get(childPosition);
+        }
+
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        @Override
+        public View getRealChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+            ChildHolder holder;
+            ChildItem item = getChild(groupPosition, childPosition);
+            if (convertView == null) {
+                holder = new ChildHolder();
+                convertView = inflater.inflate(R.layout.list_item, parent, false);
+                holder.title = (TextView) convertView.findViewById(R.id.textTitle);
+                holder.hint = (TextView) convertView.findViewById(R.id.textHint);
+                convertView.setTag(holder);
+            } else {
+                holder = (ChildHolder) convertView.getTag();
+            }
+
+            holder.title.setText(item.title);
+            holder.hint.setText(item.hint);
+
+            return convertView;
+        }
+
+        @Override
+        public int getRealChildrenCount(int groupPosition) {
+            return items.get(groupPosition).items.size();
+        }
+
+        @Override
+        public GroupItem getGroup(int groupPosition) {
+            return items.get(groupPosition);
+        }
+
+        @Override
+        public int getGroupCount() {
+            return items.size();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+            GroupHolder holder;
+            GroupItem item = getGroup(groupPosition);
+            if (convertView == null) {
+                holder = new GroupHolder();
+                convertView = inflater.inflate(R.layout.group_item, parent, false);
+                holder.title = (TextView) convertView.findViewById(R.id.textTitle);
+                holder.imageView =(ImageView) convertView.findViewById(R.id.indicator);
+                convertView.setTag(holder);
+            } else {
+                holder = (GroupHolder) convertView.getTag();
+            }
+
+            holder.title.setText(item.title);
+            if (isExpanded) {
+                holder.imageView.setImageResource(R.drawable.ic_launcher);
+            } else {
+                holder.imageView.setImageResource(R.drawable.abc_ic_clear_mtrl_alpha);
+            }
+            return convertView;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public boolean isChildSelectable(int arg0, int arg1) {
+            return true;
+        }
+
     }
 }
